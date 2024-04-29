9ad1e9ea9caa32d530c3e4c0862d6d9dde132812
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 17:29:51 -0400

    dashboard page for using the app

---
+++ pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>dashboard</h1>
+</template>
+
+<script setup lang="ts">
+import { getRefreshToken } from '~/utils/getRefreshToken'
+
+const redirectUser = getRefreshToken()
+
+</script>F
