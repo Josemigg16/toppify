b1a79d8f9564bdaf3a93748c1eb5caf9b0b5a22f
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 17:29:35 -0400

    auth page for save access and refresh token

---
+++ pages/auth.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const route = useRoute()
+const accessToken = route.query.access_token as | null | undefined
+const refreshToken = route.query.refresh_token as | null | undefined
+console.log(route.query)
+window
+if (typeof window !== 'undefined') {
+  localStorage.setItem('access_token', accessToken || '')
+  localStorage.setItem('refresh_token', refreshToken || '')
+  window.location.replace(config.public.baseUrl + '/dashboard')
+}
+
+</script>
