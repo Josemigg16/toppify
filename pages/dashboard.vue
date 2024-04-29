e4971bcc967af3fe834b02dde6c49f3694868028
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 19:41:45 -0400

    add verify token in dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const config = useRuntimeConfig()
+const redirect = ref(false)
+
+onNuxtReady(async () => {
+  const success = await getRefreshToken() as boolean
+  redirect.value = success ? true : false
+  if (redirect.value) window.location.replace(config.public.baseUrl)
+  getTopArtist()
+})
+
+
+</script>
+
 <template>
   <h1>dashboard</h1>
 </template>
-
-<script setup lang="ts">
-import { getRefreshToken } from '~/utils/getRefreshToken'
-
-const redirectUser = getRefreshToken()
-
-</script>F
