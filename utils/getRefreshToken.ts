fd1eec4a19897e7bf02d09dcae32429ef333fa93
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 19:40:36 -0400

    fix getRefreshToken for using api route

--- utils/getRefreshToken.ts
+++ utils/getRefreshToken.ts
@@ -1,24 +1,15 @@
-import querystring from "querystring"
-
 export async function getRefreshToken() {
   const config = useRuntimeConfig()
-  const refreshToken = localStorage.getItem("refresh_token")
-  const url = "https://accounts.spotify.com/api/token"
+  const refreshToken = localStorage.getItem("refresh_token") as string
 
-  const payload = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token: refreshToken,
-      client_id: config.public.clientId,
-    }),
-  }
-  const res = await fetch(url, payload)
+  console.log(refreshToken)
+  console.log(config.public.clientId)
+
+  const res = await fetch('api/refresh?refresh_token=' + refreshToken)
   const data = await res.json()
-  if (!data.access_token) return false
-  localStorage.setItem("access_token", data.accessToken)
-  localStorage.setItem("refresh_token", data.refreshToken)
+
+  console.log(data)
+  if (!data.access_token) return true
+  localStorage.setItem("access_token", data.access_token)
+  return false
 }
