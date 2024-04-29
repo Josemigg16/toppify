246657b45b72da9d063e39740d9fd899e6dfc4ac
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 17:30:22 -0400

    create login api route

---
+++ server/api/login.ts
@@ -0,0 +1,20 @@
+import querystring from "querystring"
+import { generateRandomString } from "~/utils/generateRandomString"
+
+export default defineEventHandler(({ node }) => {
+  const config = useRuntimeConfig()
+  const state = generateRandomString(16)
+  const scope = "user-read-private user-read-email"
+  node.res.writeHead(301, {
+    location:
+      "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: config.clientId,
+        scope: scope,
+        redirect_uri: config.redirectUri,
+        state: state,
+      }),
+  })
+  node.res.end()
+})
