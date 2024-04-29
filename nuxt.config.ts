b5a5668fa726feeed7259c90d9840b1f24b3a4c9
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 17:30:11 -0400

    env config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss', "@nuxt/eslint"],
+	runtimeConfig: {
+		clientId: '',
+		clientSecret: '',
+		redirectUri: '',
+		public: {
+			clientId: '',
+			baseUrl: '',
+		}
+	}
 })
