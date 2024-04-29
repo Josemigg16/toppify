7ba1736fc6dfba888c9423604132dbcabfdfa00f
Jose Miguel <josemiggomez16@gmail.com>
2024-04-29 17:30:30 -0400

    create callback api route

---
+++ server/api/callback.ts
@@ -0,0 +1,39 @@
+import querystring from 'querystring'
+
+export default defineEventHandler(async (event) => {
+	const config = useRuntimeConfig()
+	const query = getQuery(event)
+	const code = (query.code ?? null) as string | null
+	const state = query.state ?? null
+	if (state === null) {
+		event.node.res.writeHead(
+			301,
+			'/#' +
+				querystring.stringify({
+					error: 'state_mismatch'
+				})
+		)
+		event.node.res.end()
+	} else {
+		const res = await fetch('https://accounts.spotify.com/api/token', {
+			body: querystring.stringify({
+				code,
+				redirect_uri: config.redirectUri,
+				grant_type: 'authorization_code'
+			}),
+			headers: {
+				'content-type': 'application/x-www-form-urlencoded',
+				Authorization:
+					'Basic ' + Buffer.from(config.clientId + ':' + config.clientSecret).toString('base64')
+			},
+			method: 'POST'
+		})
+		const data = await res.json()
+		if (data.access_token) {
+			event.node.res.writeHead(301, {
+				location: config.public.baseUrl + '/auth?' + querystring.stringify(data)
+			})
+			event.node.res.end()
+		}
+	}
+})
