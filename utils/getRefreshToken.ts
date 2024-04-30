af026d7bc865567b35c3bf3a903ef9f3c4099a89
Jose Miguel <josemiggomez16@gmail.com>
2024-04-30 01:28:57 -0400

    format

--- utils/getRefreshToken.ts
+++ utils/getRefreshToken.ts
@@ -1,15 +1,15 @@
 export async function getRefreshToken() {
-  const config = useRuntimeConfig()
-  const refreshToken = localStorage.getItem("refresh_token") as string
+	const config = useRuntimeConfig()
+	const refreshToken = localStorage.getItem('refresh_token') as string
 
-  console.log(refreshToken)
-  console.log(config.public.clientId)
+	console.log(refreshToken)
+	console.log(config.public.clientId)
 
-  const res = await fetch('api/refresh?refresh_token=' + refreshToken)
-  const data = await res.json()
+	const res = await fetch('api/refresh?refresh_token=' + refreshToken)
+	const data = await res.json()
 
-  console.log(data)
-  if (!data.access_token) return true
-  localStorage.setItem("access_token", data.access_token)
-  return false
+	console.log(data)
+	if (!data.access_token) return true
+	localStorage.setItem('access_token', data.access_token)
+	return false
 }
