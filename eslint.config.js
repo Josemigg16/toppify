62ff7f8e45d6593721875b29922d5a5330235d78
Jose Miguel <josemiggomez16@gmail.com>
2024-04-30 01:28:46 -0400

    format

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default nuxt({
-  files: ['**/*.ts', '**/*.tsx'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'semi': ["error", "never"],
-  },
+	files: ['**/*.ts', '**/*.tsx'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'warn',
+		semi: ['error', 'never']
+	}
 })
