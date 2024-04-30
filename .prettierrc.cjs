6a415dd66d13b83083e7054ddfa296c75eb09502
Jose Miguel <josemiggomez16@gmail.com>
2024-04-30 01:27:56 -0400

    add prettier configs

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,20 @@ const { rules } = require("eslint-config-prettier")
 
 module.exports = {
   ...rules,
+  printWidth: 100,
   semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: true,
   overrides: [
     {
-      files: "*.vue",
+      files: ["*.json", "*.md", "*.toml", "*.yml", "*.vue"],
+      options: {
+        useTabs: false,
+      },
     },
   ],
+  endOfLine: "lf",
 }
