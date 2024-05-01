fd3d0d19aa5c7477ad17ee548636a5dedbd10262
Jose Miguel <josemiggomez16@gmail.com>
2024-04-30 20:13:00 -0400

    fix responsive

--- components/Tracks/ShowTracks.vue
+++ components/Tracks/ShowTracks.vue
@@ -5,9 +5,9 @@ const itemsStore = useItemsStore()
 const { items } = storeToRefs(itemsStore)
 </script>
 <template>
-    <section class="w-screen p-24">
+    <section class="w-screen p-24 grid place-content-center">
         <GoBack />
-        <section class="mx-auto w-fit">
+        <section class="w-fit">
             <TrackRow v-for="item, index in items" :key="index" :item="item" />
         </section>
     </section>
--- components/Tracks/TrackRow.vue
+++ components/Tracks/TrackRow.vue
@@ -12,12 +12,12 @@ const href = external_urls.spotify
 </script>
 
 <template>
-    <article class="flex place-content-center p-3 w-[1000px] bg-white dark:bg-[--spotify-gray] h-40 mb-6 rounded">
-        <a :href="href" target="_blank" class="block h-fit shadow-md">
-            <img class="h-32" :src="image" :alt="image + ' image'">
+    <article class="flex place-content-center p-3 w-[1000px] max-w-[90vw] bg-white dark:bg-[--spotify-gray] sm:h-40 h-24 mb-6 rounded mx-auto">
+        <a :href="href" target="_blank" class="shadow-md min-w-20">
+            <img class="sm:h-32 h-20 min-w-20" :src="image" :alt="image + ' image'">
         </a>
-        <div class="flex justify-center items-center flex-grow">
-            <h3 class="text-xl">{{ name }}</h3>
+        <div class="flex justify-center items-center flex-grow overflow-hidden">
+            <h3 class="text-xl overflow-hidden text-ellipsis text-nowrap px-1">{{ name }}</h3>
         </div>
     </article>
 </template>
